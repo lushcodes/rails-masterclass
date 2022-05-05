1cce10077de25d8867850f0c77ba2a8d0cda9601
lushcodes <lushcodes@gmail.com>
2022-05-05 13:23:02 +0100

    Authenticator challenge

---
+++ 2_ruby_language/Challenges/authenticator.rb
@@ -0,0 +1,35 @@
+users = [
+  {username: 'Gary', password: 'password1'},
+  {username: 'Mary', password: 'password2'},
+  {username: 'Barry', password: 'password3'}
+]
+
+def auth_user(username, password, user_list)
+  user_list.each do |user|
+    if user[:username] == username && user[:password] == password
+      return user
+    end
+  end
+  "Incorrect credentials"
+end
+
+puts "Welcome to the authenticator"
+25.times {print "-"}
+puts
+puts "This program will take input from the user and compare passwords. If the password is correct, you will receive the user object"
+
+attempts = 1
+while attempts < 4
+  puts "Username:"
+  input_username = gets.chomp
+  puts "Password:"
+  input_password = gets.chomp
+
+  authentication = auth_user(input_username, input_password, users)
+  puts authentication
+  puts "Press n to quit or any other key to continue"
+  continue = gets.chomp.downcase
+  break if continue == "n"
+  attempts += 1
+end
+puts "No more attempts remaining" if attempts == 4
