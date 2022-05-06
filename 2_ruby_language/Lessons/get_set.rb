e46cb2dd13a93fd1c247c12da01df081e25f61f5
lushcodes <lushcodes@gmail.com>
2022-05-06 12:54:25 +0100

    classes

---
+++ 2_ruby_language/Lessons/get_set.rb
@@ -0,0 +1,24 @@
+
+class Student
+  attr_accessor :first_name, :last_name, :email, :username, :password
+
+  def initialize(firstname, lastname, email, username, password)
+    @first_name = firstname
+    @last_name = lastname
+    @email = email
+    @username = username
+    @password = password
+  end
+
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, Email: #{@email}, Username; #{@username}"
+  end
+
+
+end
+
+luke = Student.new("Luke", "Usher", "lushcodes@gmail.com", "lushcodes", "password1")
+ian =  Student.new("Ian", "Usher", "ian@ivops.co.uk", "pompey-ian", "password1")
+
+puts luke
+puts ian
